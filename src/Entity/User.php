28ae60df0805c455654e6faec89272e888334ed0
Cryde <t.jeremy.0@gmail.com>
2018-07-21 08:32:36 +0200

    feat(#1): create the basic User Entity + migration

---
+++ src/Entity/User.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use FOS\UserBundle\Model\User as BaseUser;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+    /**
+     * @ORM\Column(type="datetime")
+     *
+     * @var \DateTime
+     */
+    protected $creationDatetime;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->creationDatetime = new \DateTime();
+    }
+}
