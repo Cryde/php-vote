7b46536af0cece7e5f89671186a21ff340077d41
Cryde <t.jeremy.0@gmail.com>
2018-08-09 08:31:56 +0200

    feat: install the doctrine extension bundle to allows use later to track changes when editing

--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,5 @@ return [
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     Exercise\HTMLPurifierBundle\ExerciseHTMLPurifierBundle::class => ['all' => true],
     FOS\UserBundle\FOSUserBundle::class => ['all' => true],
+    Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
 ];
